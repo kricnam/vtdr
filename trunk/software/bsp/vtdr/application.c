f2a1ac981c7de0269f19b5c29f9b92eb4c38d80f
me.xinxin@gmail.com <me.xinxin@gmail.com@faeaef05-255a-1c85-8150-c1329a0c1cf2>
2012-07-09 15:33:37 +0000

    switch to rtt 1.0.2,compiled by gcc

--- trunk/software/bsp/vtdr/application.c
+++ trunk/software/bsp/vtdr/application.c
@@ -1,3 +1,4 @@
+
 /*
  * File      : application.c
  * This file is part of RT-Thread RTOS
@@ -17,61 +18,156 @@
  */
 /*@{*/
 
+#include <board.h>
 #include <rtthread.h>
 
 #ifdef RT_USING_DFS
 /* dfs init */
 #include <dfs_init.h>
-/* dfs filesystem:ELM FatFs filesystem init */
+/* dfs filesystem:ELM filesystem init */
 #include <dfs_elm.h>
 /* dfs Filesystem APIs */
 #include <dfs_fs.h>
 #endif
+
+#ifdef RT_USING_LWIP
+#include <lwip/sys.h>
+#include <lwip/api.h>
+#include <netif/ethernetif.h>
+#endif
+
+#ifdef RT_USING_RTGUI
+#include <rtgui/rtgui.h>
+#include <rtgui/rtgui_server.h>
+#include <rtgui/rtgui_system.h>
+#include <rtgui/driver.h>
+#endif
+
 #include "led.h"
-#include "font_lib.h"
-#include "lcd.h"
-#include "board.h"
+
+ALIGN(RT_ALIGN_SIZE)
+static rt_uint8_t led_stack[ 512 ];
+static struct rt_thread led_thread;
+static void led_thread_entry(void* parameter)
+{
+    unsigned int count=0;
+
+//    rt_hw_led_init();
+
+    while (1)
+    {
+        /* led1 on */
+#ifndef RT_USING_FINSH
+        rt_kprintf("led on, count : %d\r\n",count);
+#endif
+        count++;
+//        rt_hw_led_on(0);
+        rt_thread_delay( RT_TICK_PER_SECOND/2 ); /* sleep 0.5 second and switch to other thread */
+
+        /* led1 off */
+#ifndef RT_USING_FINSH
+        rt_kprintf("led off\r\n");
+#endif
+//        rt_hw_led_off(0);
+        rt_thread_delay( RT_TICK_PER_SECOND/2 );
+    }
+}
 
 void rt_init_thread_entry(void* parameter)
 {
 /* Filesystem Initialization */
 #ifdef RT_USING_DFS
 	{
-        /* init the device filesystem */
-        dfs_init();
-#ifdef RT_USING_DFS_ELMFAT
-        /* init the elm FAT filesystam*/
-        elm_init();
+		/* init the device filesystem */
+		dfs_init();
 
-        /* mount sd card fat partition 1 as root directory */
-        if (dfs_mount("sd0", "/", "elm", 0, 0) == 0)
-            rt_kprintf("File System initialized!\n");
-        else
-            rt_kprintf("File System init failed!\n");
+#ifdef RT_USING_DFS_ELMFAT
+		/* init the elm chan FatFs filesystam*/
+		elm_init();
+
+		/* mount sd card fat partition 1 as root directory */
+		if (dfs_mount("sd0", "/", "elm", 0, 0) == 0)
+		{
+			rt_kprintf("File System initialized!\n");
+		}
+		else
+			rt_kprintf("File System initialzation failed!\n");
 #endif
 	}
 #endif
-        lcd_write_matrix(0,0,(unsigned char*)distance_cheng);
-        rt_hw_buzz_on();
-        rt_thread_delay(10);
-        rt_hw_buzz_off();
-        for(;;)
-        {
-          rt_hw_led_on(0);
-          volatile int n;
-          for(int i=0;i<5000;i++)
-          {
-            n++;
-          }
-          rt_hw_led_off(0);
-          rt_thread_delay(1);
-        }
-            
+
+/* LwIP Initialization */
+#ifdef RT_USING_LWIP
+	{
+		extern void lwip_sys_init(void);
+
+		/* register ethernetif device */
+		eth_system_device_init();
+
+#ifdef STM32F10X_CL
+		rt_hw_stm32_eth_init();
+#else
+	/* STM32F103 */
+	#if STM32_ETH_IF == 0
+			rt_hw_enc28j60_init();
+	#elif STM32_ETH_IF == 1
+			rt_hw_dm9000_init();
+	#endif
+#endif
+
+		/* re-init device driver */
+		rt_device_init_all();
+
+		/* init lwip system */
+		lwip_sys_init();
+		rt_kprintf("TCP/IP initialized!\n");
+	}
+#endif
+
+#ifdef RT_USING_RTGUI
+	{
+	    extern void rtgui_startup();
+	    extern void rt_hw_lcd_init();
+	    extern void rtgui_touch_hw_init(void);
+
+		rt_device_t lcd;
+
+		/* init lcd */
+		rt_hw_lcd_init();
+
+		/* init touch panel */
+		rtgui_touch_hw_init();
+
+		/* re-init device driver */
+		rt_device_init_all();
+
+		/* find lcd device */
+		lcd = rt_device_find("lcd");
+
+		/* set lcd device as rtgui graphic driver */
+		rtgui_graphic_set_device(lcd);
+
+		/* startup rtgui */
+		rtgui_startup();
+	}
+#endif /* #ifdef RT_USING_RTGUI */
 }
 
 int rt_application_init()
 {
 	rt_thread_t init_thread;
+
+	rt_err_t result;
+
+    /* init led thread */
+	result = rt_thread_init(&led_thread,
+		"led",
+		led_thread_entry, RT_NULL,
+		(rt_uint8_t*)&led_stack[0], sizeof(led_stack), 20, 5);
+	if (result == RT_EOK)
+	{
+        rt_thread_startup(&led_thread);
+	}
 
 #if (RT_THREAD_PRIORITY_MAX == 32)
 	init_thread = rt_thread_create("init",
