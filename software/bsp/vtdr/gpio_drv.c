99ed99cf782f864e08cc2aaec2a36e8e398687da
me.xinxin <me.xinxin@gmail.com>
2012-09-11 15:23:52 +0000

    external interrupt

--- software/bsp/vtdr/gpio_drv.c
+++ software/bsp/vtdr/gpio_drv.c
@@ -48,7 +48,7 @@ void rt_hw_gpio_init(void)
     GPIO_InitTypeDef GPIO_InitStructure;
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
-    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN_FLOATING;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IPU;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 
     GPIO_InitStructure.GPIO_Pin   = speed_plus_pin;
--- software/bsp/vtdr/gpio_drv.h
+++ software/bsp/vtdr/gpio_drv.h
@@ -8,6 +8,7 @@
 #ifndef GPIO_DRV_H_
 #define GPIO_DRV_H_
 
+void rt_hw_gpio_init(void);
 
 
 #endif /* GPIO_DRV_H_ */
